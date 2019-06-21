88fe9c9b6aec5935fdf1e9683037b12e67ca5276
Radek <radek3301@linux.pl>
2019-06-21 23:43:33 +0200

    first commit

---
+++ snapshots_gentoo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget http://gentoo.po.opole.pl/snapshots/portage-latest.tar.xz
+
+tar xf portage-latest.tar.xz
---
+++ stage3_gentoo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget http://gentoo.po.opole.pl/releases/x86/autobuilds/current-stage3-i686/stage3-i686-20190618T214502Z.tar.xz
+
+tar xf stage3-i686-20190618T214502Z.tar.xz
